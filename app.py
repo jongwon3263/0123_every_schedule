8e4c8e2a7dc11d077f3c3e26a9abb45640dc27aa
“jongwon3263” <jongwon3263@gmail.com>
2024-01-24 20:00:00 +0900

    sd

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ scope = [
 ]
 
 json_file_name = '/Users/gwonjong-won/Documents/0./Projects/0123_every_schedule/gspreadstreamlit-c4905c3ce953.json'
-credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
-gc = gspread.authorize(credentials)
+gc = gspread.authorize(ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope))
 spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1_IXE_zCjUANYAf2wKM0ektMTzqpn4SZIWm8Ct2WJ4xI/edit?usp=sharing'
 doc = gc.open_by_url(spreadsheet_url)
 worksheet_name = '종원작업용'
