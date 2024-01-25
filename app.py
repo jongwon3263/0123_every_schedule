e62b42c4959f380b9d698320839f534f9ef91e93
“jongwon3263” <jongwon3263@gmail.com>
2024-01-25 17:55:21 +0900

    qer

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import gspread
 import pandas as pd
 from datetime import datetime
 from oauth2client.service_account import ServiceAccountCredentials
+import json
 
 st.set_page_config(
     page_icon="🐶",
@@ -10,19 +11,6 @@ st.set_page_config(
     layout="wide"
 )
 
-st.json({
-  "type": "service_account",
-  "project_id": "gspreadtest-375317",
-  "private_key_id": "2426aad55bdac0d25845b120b2b1350734f4678c",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCedgX/SvaRM9e7\nAaRPoQduK+1eIqHOFJQXlx1YeHRPBkJfk27pHEaVk69a3238IXYwVWOtu3am6nRt\nd9tsG7PPDuKaQ9aDYYCq0/F7JP9SZRYUMUhV105MkAhX+tMgsb/K15XidZZKu498\nYLv3l7RqjgIOQaIdkVY7TmbWQ7HI8Fuo7OGOtGoQOh6m0M/DyV5Lkfg/iYgf/KzG\n1G/fZe65SxDXoOSggoNn7mhfgMNHufNjCG+rtmKmrBDrUPBha9Pg1lgQ3QeUSiiB\nDs1FXm7Zc4HA3igZInocw8m1lVwb57N7v+pl+pIFREDEWdctF/LOlHuNctnWFL2A\noH9+9CDFAgMBAAECggEAA+4meO4JjpFsHOHOCwGR9xwUz5xpSfy9rZYcbrjSHbGo\njMlR22tNnuEiXM0k47eKwGklx0XajApNp/dslFJdk9mpKi1bkTemYtZN64F/6bHD\nKe4M/sAP25tI8bGxceKmwKe3On4zI5hiVzHpB5uWKHxsNaFQHY6gxmjL6S2Vp8ew\nY+e1NyCLHUFPfE3piHgSYDFW9M1zx85AuSSGvAGSr/x8LzBy8z9Lpa+zVYH0x/hG\ncVRs597c272s1K+H6Ac7/iVZH5YcweYVjgpA122zQId7Fg70ELb44xC1458QM0FZ\ndtZRc39lKi/CGJ+ELaDIfvgT6V4XC2YIL9rNt4hOQwKBgQDV8JLmyU8kzatr132i\nEGXVuX49u07RaMLJ55qRlPo0B4fQ3L3+fCL0F6AldmwTFtCwkm8BqLu+w7F9uTBq\nNYA8Vk0u6kohQe/jECUDH7b2mrIE8mIrZZDzruRBKhkBLr9VIKqJoFRpVc+pwSRK\nmpP8LTgdstepke6kjb8qR4QQswKBgQC9nT9sbRTpoe3xKGbPxtKCdzDrZQ+MY5f7\nYM0P5Rs4rVNnuALEzVJEvrrtH9d1OQNW4Ndxlljwzd6wHf131KYOizDYXl3tzPvI\n2M7go7fmIxRjSXpQYYKgVJSncHvVAn1g7MvI5udVjqGCPhe8bTJJCV8QD/68byGs\nnQy1yKPUpwKBgQCm8Sdwt73udh9B89GxWSJd+HUuLq9+cUUu0G+pEpt7ZjTxIo+P\nucZ8usLY+OO0saT/ZAUDQmvenE4wUQYams0KSD3KUkCuWwmaIcr/+QuIu4YWnh1E\nx+ioftzWjMceSSA8iVUKN/YmjDOTv4dWP/EiYxba9eecBqg19/YoQ84mSQKBgQC2\nkzL+tAnu7HywOLbGWitYdGwmPKk8BKnS4uKQ64nKiRpGRGhmF02f5AcZTA+qT7tn\nbBgGxz3coF5MNpRZTeiXGG9v522KDFZkjl6k7v+kGSMIdzfPLhUZTFfW4fhaoosX\nSOrwB2W4fB4HysIQLRS+PsfKL5yY1GkYTCBw6UDXAQKBgFb8ejdjQIxHspQ0EihL\nJ2v8UrcDO9E1999wVgOXR7RETDaqpJn7asd0h6RULeL6krPGkuA0lexvtQOZvEnf\njJudYia1udPdIEmCvnDqiQ4VArEl+FVSk01RjmUulXtCns1kxaVMrNlACjQ6SA7r\nxnfNsl6pG2Xbn9s2KQCXz+KA\n-----END PRIVATE KEY-----\n",
-  "client_email": "gspread@gspreadtest-375317.iam.gserviceaccount.com",
-  "client_id": "105511259535492147602",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/gspread%40gspreadtest-375317.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-})
 
 scope = [
 'https://spreadsheets.google.com/feeds',
@@ -30,7 +18,8 @@ scope = [
 ]
 
 json_file_name = '/Users/gwonjong-won/Documents/0./Projects/0123_every_schedule/gspreadtest-375317-2426aad55bda.json'
-credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scopes=scope, token_uri="https://oauth2.googleapis.com/token")
+with open(json_file_name, 'r') as file_obj:
+    client_credentials = json.load(file_obj)
 
 gc = gspread.authorize(credentials)
 spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1_IXE_zCjUANYAf2wKM0ektMTzqpn4SZIWm8Ct2WJ4xI/edit?usp=sharing'
